cc21b9f54db2b61a1de9431ee3969c866ab921ac
hamza.jeridi <hamza.jeridi@CPX-658614DLLFA.dir.svc.accenture.com>
2017-03-15 17:58:37 +0100

    hjridi: commit intermediaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CCSP';
+  public data;
+  public filterQuery = "";
+  public rowsOnPage = 10;
+  public sortBy = "email";
+  public sortOrder = "asc";
+  public keySearch ; 
+ public filteredItems;
+  constructor(private http: Http) {
+  }
+
+  assignCopy():void{
+   this.filteredItems = Object.assign([], this.data);
+}
+filterItem(value){
+   if(!value) this.assignCopy(); //when nothing has typed
+   this.filteredItems = Object.assign([], this.data).filter(
+      item => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+   )
+}
+//this.assignCopy();//when you fetch collection from server.
+  
+  
+  
+  ngOnInit(): void {
+    this.http.get('app/data.json')
+      .subscribe((data) => {
+        setTimeout(() => {
+          this.data = data.json();
+        }, 1000);
+      });
+  }
+
+  public toInt(num: string) {
+    return +num;
+  }
+
+  public sortByWordLength = (a: any) => {
+    return a.city.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
+import {DataTableModule} from 'angular2-datatable';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
